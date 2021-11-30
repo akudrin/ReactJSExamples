38ccb3ba2065b8d2e821e182afa4eb27f506b3cb
Andrei Kudrin <andreikudrin@gmail.com>
2021-11-30 09:00:25 +0000

    ReactJS Beta - Web page React

---
+++ webpagereact/like_button.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function LikeButton() {
+  const [liked, setLiked] = React.useState(false);
+
+  if (liked) {
+    return 'You liked this!';
+  }
+
+  return React.createElement(
+    'button',
+    {
+      onClick: () => setLiked(true),
+    },
+    'Like'
+  );
+}
+const domContainer = document.getElementById('component-goes-here');
+ReactDOM.render(React.createElement(LikeButton), domContainer);
