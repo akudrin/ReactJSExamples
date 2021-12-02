5340e177856c56bf855331199b1bcc62a99566ce
Andrei Kudrin <andreikudrin@gmail.com>
2021-12-02 09:03:56 +0000

    MaterialUI - mui system basics

--- MaterialUI/my-app-react/src/App.js
+++ MaterialUI/my-app-react/src/App.js
@@ -4,8 +4,10 @@ function App() {
   return (
     <div className='App'>
       <h1>Material UI</h1>
-      <Typography>Hello World</Typography>
-      <Button variant='outlined' color='primary'>
+      <Typography sx={{ color: 'red', fontSize: 40, fontWeight: 'bold' }}>
+        Hello World
+      </Typography>
+      <Button sx={{ boxShadow: 5 }} variant='outlined' color='primary'>
         First Button
       </Button>
       <Button variant='text'>Text</Button>
