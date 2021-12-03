609857a2c821362349679cb4d230f85a9f494124
Andrei Kudrin <andreikudrin@gmail.com>
2021-12-03 14:23:52 +0000

    MaterialUI - Components/Buttons

---
+++ MaterialUI/mui components/src/App.js
@@ -0,0 +1,13 @@
+import ButtonSizes from './components/ButtonSizes';
+import IconButtonColors from './components/IconButtonColors';
+import IconButtons from './components/IconButtons';
+
+function App() {
+  return (
+    <div>
+      <IconButtonColors /> <IconButtons /> <ButtonSizes />
+    </div>
+  );
+}
+
+export default App;
